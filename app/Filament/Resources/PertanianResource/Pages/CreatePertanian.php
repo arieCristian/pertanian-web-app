84368b4e3b3811d2527156af03882246ea9de906
ArieCristian <mdariecristian@gmail.com>
2024-09-05 21:40:21 +0700

    fix list tanaman issue

--- app/Filament/Resources/PertanianResource/Pages/CreatePertanian.php
+++ app/Filament/Resources/PertanianResource/Pages/CreatePertanian.php
@@ -11,16 +11,4 @@ use Filament\Resources\Pages\CreateRecord;
 class CreatePertanian extends CreateRecord
 {
     protected static string $resource = PertanianResource::class;
-
-    protected function handleRecordCreation(array $data): Pertanian
-    {
-        $list_tanaman = $data['list_tanaman'];
-        unset($data['list_tanaman']);
-        $record = Pertanian::create($data);
-        foreach ($list_tanaman as $tanaman) {
-            $tanaman['pertanian_id'] = $record->id;
-            JumTanaman::create($tanaman);
-        }
-        return $record;
-    }
 }
